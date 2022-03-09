294698f9664f35aa50e0fc2055c29bbbc95f2e24
gammbol <gammbol2525@gmail.com>
2022-03-09 21:26:27 +0000

    step 5 (without update README)

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import askName from '../src/cli.js';
+import { playGame } from '../src/games/even.js';
 
 console.log('Welcome to the Brain Games!');
 const name = askName();
 console.log(`Hello, ${name}`);
+
+playGame(name);
---
+++ src/games/even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+export const getRandomInt = () => {
+  const ranNum = Math.floor(Math.random() * 100);
+  return ranNum;
+};
+
+export const checkIntAns = (num, ans) => {
+  const ansBull = ans === 'yes';
+  if ((num % 2 === 0) === ansBull) {
+    return true;
+  }
+  return false;
+};
+
+export const playGame = (name) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const ranInt = getRandomInt();
+    console.log(`Question: ${ranInt}`);
+
+    const ans = readlineSync.question('Your answer: ');
+
+    if (checkIntAns(ranInt, ans)) {
+      console.log('Correct!');
+    } else {
+      const contrAns = (ans === 'yes') ? 'no' : 'yes';
+      console.log(`'${ans}' is wrong ;(. Correct answer was '${contrAns}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
