5b73a41f03c40acc050db1203f726f776e5b88eb
gammbol <gammbol2525@gmail.com>
2022-03-11 19:49:27 +0000

    reconfigure cli.js and update brain-games.js and brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,8 +2,4 @@
 import askName from '../src/cli.js';
 import { playGame } from '../src/games/even.js';
 
-console.log('Welcome to the Brain Games!');
-const name = askName();
-console.log(`Hello, ${name}`);
-
-playGame(name);
+playGame(askName());
