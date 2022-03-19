53030a4d2737e7954b3b428f4036df7ed0e70409
gammbol <gammbol2525@gmail.com>
2022-03-19 23:47:45 +0000

    step 6

--- src/games/even.js
+++ src/games/even.js
@@ -4,18 +4,19 @@ const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export default () => {
   const name = utils.welcome(rules);
+  let correct = true;
 
   for (let i = 0; i < utils.roundNum; i += 1) {
-    const question = utils.getRandomInt();
+    const question = utils.getRandomInt(0, 100);
     const answer = utils.askQuestion(`${question}`);
 
     const rightAns = utils.isEven(question, answer);
     if (rightAns === true) console.log('Correct!');
     else {
       utils.lose(name, answer, rightAns);
+      correct = false;
       break;
     }
-
-    utils.win(name);
   }
+  if (correct) utils.win(name);
 };
